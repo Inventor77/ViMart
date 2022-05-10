b213c072d614753b5211f7afc6df25cfdc6f9a7a
Shreedutt <shreedutt77@gmail.com>
2022-05-10 22:42:11 +0530

    Some global styles are added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Nav.scss'
+
 
 function Nav() {
     return (
-        <div>
-
+        <div className='nav'>
+            <div className='logo_container'>
+                {/* <img src={Logo} alt='logo' /> */}
+            </div>
         </div>
     )
 }
