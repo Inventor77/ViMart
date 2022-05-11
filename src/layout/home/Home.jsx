5a6de314d5d079d2016caceeb85ad367ff04c958
Shreedutt <shreedutt77@gmail.com>
2022-05-11 20:10:53 +0530

    Data Fecting functionality is added

--- src/layout/home/Home.jsx
+++ src/layout/home/Home.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from '../../components/Nav/Nav'
 import './Home.scss'
 
+
 function Home() {
+    const [q, setQ] = useState("sam")
+    const [numResults, setNumResults] = useState(5);
+    const [cardData, setCardData] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    const fetchData = async () => {
+        const url = `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${q}&numResults=${numResults}`;
+        try {
+            let res = await fetch(url);
+            let data = await res.json();
+            setCardData(data);
+            setLoading(false);
+        } catch (error) {
+            setError(error)
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
     return (
         <div>
             <Nav />
             <section className='card_container'>
-                
+                {loading && (
+                    <div className='loading'>
+                        Loading<span>.</span><span>.</span><span>.</span><span>.</span>
+                    </div>
+                )}
+                {error && (
+                    <div className='error'>
+                        {error}
+                    </div>
+                )}
+                {
+                    <pre style={{ color: "#fff" }}>{JSON.stringify(cardData.results, null, 3)}</pre>
+                }
             </section>
         </div>
     )
